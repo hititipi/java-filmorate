42aa6830821b89904a1f7bb420d6dd05930755d9
Ruslan Sabirov <r.sabirov@wisetech.pro>
2023-04-21 11:27:44 +0300

    fix: codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/AbstractInMemoryStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/AbstractInMemoryStorage.java
@@ -17,7 +17,7 @@ import static ru.yandex.practicum.filmorate.exception.ValidationErrors.RESOURCE_
 @Slf4j
 public class AbstractInMemoryStorage<T extends Resource> implements ResourceStorage<T> {
 
-    final protected Map<Integer, T> resources = new HashMap<>();
+    protected final Map<Integer, T> resources = new HashMap<>();
     private int idCounter = 1;
 
     boolean contains(T resource) {
